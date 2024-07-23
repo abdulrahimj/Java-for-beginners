04a9503bfb6e616be1e670aa60161f0821008a79
Abdulrahim <143444893+abdulrahimj@users.noreply.github.com>
2024-07-23 13:05:43 +0000

    Update InputHelper.java

--- fuelStation/src/fuelStationPumb/InputHelper.java
+++ fuelStation/src/fuelStationPumb/InputHelper.java
@@ -3,7 +3,8 @@ package fuelStationPumb;
 //
 // Source code recreated from a .class file by IntelliJ IDEA
 // (powered by FernFlower decompiler)
-//
+
+//This is a global Scanner class for all classes. No need to create a scanner in each class
 
 import java.util.Scanner;
 
